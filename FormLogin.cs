9a5b54c60272c7e1727a35f4b41658439fa8be25
Eka051 <dianekaraharjo@gmail.com>
2025-05-17 18:35:50 +0700

    Add project files.

---
+++ FormLogin.cs
@@ -0,0 +1,95 @@
+﻿using Npgsql;
+
+namespace Praktikum_PBO_Winform
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+            tbPassword.UseSystemPasswordChar = true;
+        }
+
+        private void BtnShowPassword_Click_1(object sender, EventArgs e)
+        {
+            bool isPasswordHidden = tbPassword.UseSystemPasswordChar;
+            tbPassword.UseSystemPasswordChar = !isPasswordHidden;
+            tbPassword.PasswordChar = isPasswordHidden ? '\0' : '●';
+
+            Image icon = isPasswordHidden ? Properties.Resources.eye_off : Properties.Resources.eye_on;
+            BtnShowPassword.BackgroundImage = icon;
+            BtnShowPassword.BackgroundImageLayout = ImageLayout.Zoom;
+        }
+
+        public bool Validate(string username, string password, out int userId)
+        {
+            userId = 0;
+            string connStr = "Host=localhost;Username=postgres;Password=dianeka@05;Database=PBO_CRUD";
+            string query = "SELECT user_id, username FROM users WHERE username = @username AND password = @password";
+            using (NpgsqlConnection conn = new NpgsqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@username", username);
+                        cmd.Parameters.AddWithValue("@password", password);
+                        using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                userId = reader.GetInt32(0);
+                                return true;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Terjadi kesalahan saat memvalidasi login: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            return false;
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (tbUsername != null && tbPassword != null)
+                {
+                    string username = tbUsername.Text;
+                    string password = tbPassword.Text;
+                    if (Validate(username, password, out int userId))
+                    {
+                        MessageBox.Show("Login berhasil. Klik OK untuk melanjutkan", "Berhasil", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        FormProduk formProduk = new FormProduk(userId);
+                        this.Hide();
+                        formProduk.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Email atau password salah", "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Username & Password harus diisi!", "Perhatian!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show("Terjadi kesalahan saat menghubungi database: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void RegisterLink_LinkClicked(object sender, EventArgs e)
+        {
+            this.Hide();
+            FormRegister formRegister = new FormRegister();
+            formRegister.Show();
+        }
+    }
+}
---
+++ FormProduk.cs
@@ -0,0 +1,106 @@
+﻿using Npgsql;
+using System.Data;
+using System.Data.Common;
+
+namespace Praktikum_PBO_Winform
+{
+    public partial class FormProduk : Form
+    {
+        private string connStr = "Host=localhost;Username=postgres;Password=dianeka@05;Database=PBO_CRUD";
+        private int _userId;
+        public FormProduk(int userId)
+        {
+            InitializeComponent();
+            _userId = userId;
+            LoadProduk();
+        }
+
+        public void TambahProduk(string nama, int harga, int stok)
+        {
+            string query = "INSERT INTO products (nama_produk, harga_produk, stok, user_id) VALUES (@nama, @harga, @stok, @userId)";
+            using (NpgsqlConnection conn = new NpgsqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@nama", nama);
+                        cmd.Parameters.AddWithValue("@harga", harga);
+                        cmd.Parameters.AddWithValue("@stok", stok);
+                        cmd.Parameters.AddWithValue("@userId", _userId);
+                        cmd.ExecuteNonQuery();
+                    }
+                    MessageBox.Show("Produk berhasil ditambahkan!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadProduk(); 
+                }
+                catch (NpgsqlException ex)
+                {
+                    MessageBox.Show("Terjadi kesalahan saat menambahkan produk: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Terjadi kesalahan tidak terduga: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void BtnTambahProduk_Click(object sender, EventArgs e)
+        {
+            string nama = tbNamaProduk.Text;
+            int harga, stok;
+            bool isHargaValid = int.TryParse(tbHargaProduk.Text, out harga);
+            bool isStokValid = int.TryParse(tbStokProduk.Text, out stok);
+
+            if (string.IsNullOrEmpty(nama) || !isHargaValid || !isStokValid || harga <= 0 || stok <= 0)
+            {
+                MessageBox.Show("Silakan isi semua field dengan benar. Pastikan harga dan stok berupa angka positif.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            TambahProduk(nama, harga, stok);
+            tbNamaProduk.Clear();
+            tbHargaProduk.Clear();
+            tbStokProduk.Clear();
+        }
+
+        public void LoadProduk()
+        {
+            dataGridView1.Rows.Clear();
+            string query = "SELECT product_id, nama_produk, harga_produk, stok FROM products WHERE user_id = @userId";
+
+            using (NpgsqlConnection conn = new NpgsqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@userId", _userId);
+
+                        using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                int productId = reader.GetInt32(0);
+                                string namaProduk = reader.GetString(1);
+                                int hargaProduk = reader.GetInt32(2);
+                                int stok = reader.GetInt32(3);
+
+                                dataGridView1.Rows.Add(productId, namaProduk, hargaProduk, stok);
+                            }
+                        }
+                    }
+                }
+                catch (NpgsqlException ex)
+                {
+                    MessageBox.Show("Terjadi kesalahan saat memuat produk: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Terjadi kesalahan tidak terduga: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
